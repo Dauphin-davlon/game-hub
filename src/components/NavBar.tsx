58d9378d43194781b13fdb0db3c5cab0788b984c
david <dauphindavlon@gmail.com>
2024-01-18 09:48:55 -0500

    fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,15 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import Logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
+import HomePage from "../pages/HomePage";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={Logo} boxSize="60px"></Image>
+      <Link to="/">
+        <Image src={Logo} boxSize="60px" objectFit="cover"></Image>
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,13 +1,10 @@
-import { Button, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
-import { useState } from "react";
 import ExpandableText from "../components/ExpandableText";
-import DefinitionItem from "../components/DefinitionItem";
-import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
-import GameTrailers from "../components/GameTrailers";
 import GameScreenshots from "../components/GameScreenshots";
+import GameTrailers from "../components/GameTrailers";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -19,13 +16,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailers gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailers gameId={game.id} />
+        <GameScreenshots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
