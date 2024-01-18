01a97eeaf18352eba7157aa46c92cd78923f39c2
david <dauphindavlon@gmail.com>
2024-01-17 23:21:13 -0500

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,11 +1,15 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 
 export interface Game {
     id: number;
     name: string;
+    genres: Genre[];
     background_image: string;
     slug: string;
+    publishers: Publisher[];
     description_raw: string;
     parent_platforms: { platform: Platform; }[];
     metacritic: number;
